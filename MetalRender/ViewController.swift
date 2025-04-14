67d17f5a65d055462846b8c6e1b0d2f74b8cdff9
016270 <18205186136@163.com>
2025-04-14 11:13:05 +0800

    Initial Commit

---
+++ MetalRender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetalRender
+//
+//  Created by Yang Long on 2025/4/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
