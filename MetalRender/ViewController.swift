40bf2680160ff3194404f6526d80141af7458221
016270 <18205186136@163.com>
2025-04-14 14:30:44 +0800

    #XZL-153# 擦除颜色

--- MetalRender/ViewController.swift
+++ MetalRender/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
+    
+    var mtkView: MTKView!
+    var render: YLRenderer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mtkView = MTKView(frame:self.view.bounds)
+        self.view.addSubview(mtkView)
+        
+        // 仅在需要更新内容时（例如当视图形状改变时）才进行绘制
+        mtkView.enableSetNeedsDisplay = true
+        
+        mtkView.device = MTLCreateSystemDefaultDevice()
+        
+        mtkView.clearColor = MTLClearColorMake(1.0, 0.5, 1.0, 1.0)
+        
+        render = YLRenderer(mtkView: mtkView)
+        
+        render.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+        
+        mtkView.delegate = render
     }
-
-
 }
 
